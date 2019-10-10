94f2fa50f68c793077037726a88224164ca4fc96
K-August <klieder03@gmail.com>
2019-10-10 10:32:24 -0500

    Beginnings of a JFrame

--- src/com/wtv/main/Main.java
+++ src/com/wtv/main/Main.java
@@ -1,1 +1,29 @@
+package com.wtv.main;
 
+import javax.swing.*;
+
+public class Main extends JFrame implements Runnable
+{
+    public static final int WIDTH = 600, HEIGHT = 800;
+    private boolean running;
+
+    public Main()
+    {
+        super("Maze Generator");
+
+        setBounds(0, 0, WIDTH, HEIGHT);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args)
+    {
+        new Main();
+    }
+
+    public void run() {
+
+    }
+}
