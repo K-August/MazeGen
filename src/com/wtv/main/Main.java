c59c1e7d80f48eb816781a30a5e207ab0a281507
Richard Davidson <richarddavidson000@gmail.com>
2019-10-10 22:20:23 -0500

    scratch that homie, now we got buttons

--- src/com/wtv/main/Main.java
+++ src/com/wtv/main/Main.java
@@ -2,6 +2,8 @@ package com.wtv.main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferStrategy;
 
 public class Main extends JFrame implements Runnable {
@@ -9,6 +11,9 @@ public class Main extends JFrame implements Runnable {
 
     private Thread thread;
     private boolean running;
+
+    JButton start = new JButton("Begin");
+    JButton exit = new JButton("Quit");
 
     public Main() {
         super("Maze Gen Thing");
@@ -18,9 +23,35 @@ public class Main extends JFrame implements Runnable {
         setResizable(false);
         setLocationRelativeTo(null);
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+        setLayout(null);
 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        start.setSize(200, 50);
+        start.setLocation(WIDTH / 2 - 100, HEIGHT - 200);
+        start.setBackground(Color.BLACK);
+        start.setForeground(Color.WHITE);
+        add(start);
+        start.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("yo! you clicked me!");
+            }
+        });
+
+        exit.setSize(150, 50);
+        exit.setLocation(WIDTH / 2 - 75, HEIGHT - 125);
+        exit.setBackground(Color.BLACK);
+        exit.setForeground(Color.WHITE);
+        add(exit);
+        exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("you trynna leave?!");
+            }
+        });
+
+        setVisible(true);
         this.start();
     }
 
@@ -39,26 +70,17 @@ public class Main extends JFrame implements Runnable {
 
     private void tick() { }
 
-    private void render() {
-        BufferStrategy bs = this.getBufferStrategy();
-        if(bs == null) {
-            this.createBufferStrategy(3);
-            return;
-        }
-
-        Graphics g = bs.getDrawGraphics();
-
-        g.setColor(Color.BLACK);
-        g.fillRect(0, 0, WIDTH, HEIGHT);
-
-        g.dispose();
-        bs.show();
+    private void renderComponent() {
+        repaint();
     }
 
     public void run() {
+        long past = System.currentTimeMillis();
         while(running) {
+            long now = System.currentTimeMillis();
             tick();
-            render();
+            if((now - past) / 1000 == 60)
+                renderComponent();
         }
         stop();
     }
